2ad1661168ac8cf7c969b6873f99c9139ae79817
Mamidala <rajendhar.mamidala@capgemini.com>
2020-03-29 10:28:04 +0800

    mixed content issue fixed

--- src/app/example-search/service/search.service.ts
+++ src/app/example-search/service/search.service.ts
@@ -10,7 +10,7 @@ export class SearchService {
   constructor(private http: HttpClient) {}
 
   getResultsByKeyWord(key): Observable<any> {
-    const ghUsers = `http://api.github.com/search/users?q=${key}`;
+    const ghUsers = `https://api.github.com/search/users?q=${key}`;
     return this.http.get(ghUsers).pipe(
       map(res => {
         return res;
